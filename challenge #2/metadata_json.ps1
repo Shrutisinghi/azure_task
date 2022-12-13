bbf168e408539be78c8cfa24b77844201b0197e1
Shrutisinghi <120420405+Shrutisinghi@users.noreply.github.com>
2022-12-14 00:58:45 +0530

    Add files via upload

---
+++ challenge #2/metadata_json.ps1
@@ -0,0 +1,8 @@
+﻿$metadata_json = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -NoProxy -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | ConvertTo-Json -Depth 64
+
+$metadata_obj = $metadata_json | ConvertFrom-Json
+
+$metadata_obj.compute
+
+
+
